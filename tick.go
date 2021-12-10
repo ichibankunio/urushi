82ead32da27e630c99c997355747f1373028325b
Ichiban Kunio <kunio.ichiban@gmail.com>
2021-12-10 21:41:46 +0900

    fixed

---
+++ tick.go
@@ -0,0 +1,58 @@
+package urushi
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+type TickF struct {
+	span int
+	content func(int, ...interface{}) bool
+	contentCounter int
+	repeat int
+	tweener Tweener
+	content2 func(int, ...TickOptions) bool
+}
+
+type TickOptions struct {
+	target interface{}
+	tweener Tweener
+}
+
+func (t *TickF) Update(g *Game, i ...interface{}) {
+	if g.counter % t.span == 0 {
+		if ebiten.MaxTPS() == 30 {
+			t.content(t.contentCounter, i)
+		}
+		if t.content(t.contentCounter, i) {
+			// t.rewind()
+		}else {
+			t.contentCounter++
+		}
+	}
+}
+
+func NewTickF(span int, content func(int, ...interface{}) bool) *TickF {
+	return &TickF{
+		span: span,
+		content: content,
+	}
+}
+
+// func NewTickF2(span int, content func (int, ...TickOptions) bool) *TickF {
+// 	return &TickF{
+// 		span: span,
+// 		content2: content,
+// 	}
+// }
+
+// func (t *TickF) Update2(g *Game, i ...interface{}) {
+// 	if g.counter % t.span == 0 {
+// 		if ebiten.MaxTPS() == 30 {
+// 			t.content(t.contentCounter, i)
+// 		}
+// 		if t.content(t.contentCounter, i) {
+// 			// t.rewind()
+// 		}else {
+// 			t.contentCounter++
+// 		}
+// 	}
+// }
+
--- tickif.go
+++ tickif.go
@@ -1,33 +1,32 @@
 package urushi
 
-import "github.com/hajimehoshi/ebiten/v2"
+// import "github.com/hajimehoshi/ebiten/v2"
 
 
-type TickF struct {
-	span int
-	content func(int, ...interface{}) bool
-	contentCounter int
-	repeat int
-}
+// type TickF struct {
+// 	span int
+// 	content func(int, ...interface{}) bool
+// 	contentCounter int
+// 	repeat int
+// }
 
-func NewTickF(span int, content func(int, ...interface{}) bool) *TickF {
-	return &TickF{
-		span: span,
-		content: content,
-	}
-}
+// func NewTickF(span int, content func(int, ...interface{}) bool) *TickF {
+// 	return &TickF{
+// 		span: span,
+// 		content: content,
+// 	}
+// }
 
-func (t *TickF) Update(g *Game, i interface{}) {
-	if g.counter % t.span == 0 {
-		if ebiten.MaxTPS() == 30 {
-			t.content(t.contentCounter, i)
-		}
-		if t.content(t.contentCounter, i) {
-			// t.rewind()
-		}else {
-			t.contentCounter++
-		}
-	}
+// func (t *TickF) Update(g *Game, i interface{}) {
+// 	if g.counter % t.span == 0 {
+// 		if ebiten.MaxTPS() == 30 {
+// 			t.content(t.contentCounter, i)
+// 		}
+// 		if t.content(t.contentCounter, i) {
+// 			// t.rewind()
+// 		}else {
+// 			t.contentCounter++
+// 		}
+// 	}
 	
-}
-
+// }
