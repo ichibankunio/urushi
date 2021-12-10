171318521834119004ba840aa8f47ccb32158a87
Ichiban Kunio <kunio.ichiban@gmail.com>
2021-12-10 21:38:12 +0900

    fixed

--- tick.go
+++ tick.go
@@ -7,6 +7,13 @@ type TickF struct {
 	content func(int, ...interface{}) bool
 	contentCounter int
 	repeat int
+	tweener Tweener
+	content2 func(int, ...TickOptions) bool
+}
+
+type TickOptions struct {
+	target interface{}
+	tweener Tweener
 }
 
 func (t *TickF) Update(g *Game, i ...interface{}) {
@@ -20,7 +27,6 @@ func (t *TickF) Update(g *Game, i ...interface{}) {
 			t.contentCounter++
 		}
 	}
-	
 }
 
 func NewTickF(span int, content func(int, ...interface{}) bool) *TickF {
@@ -29,4 +35,24 @@ func NewTickF(span int, content func(int, ...interface{}) bool) *TickF {
 		content: content,
 	}
 }
+
+// func NewTickF2(span int, content func (int, ...TickOptions) bool) *TickF {
+// 	return &TickF{
+// 		span: span,
+// 		content2: content,
+// 	}
+// }
+
+// func (t *TickF) Update2(g *Game, i ...interface{}) {
+// 	if g.counter % t.span == 0 {
+// 		if ebiten.MaxTPS() == 30 {
+// 			t.content(t.contentCounter, i)
+// 		}
+// 		if t.content(t.contentCounter, i) {
+// 			// t.rewind()
+// 		}else {
+// 			t.contentCounter++
+// 		}
+// 	}
+// }
 
---
+++ tweener.go
@@ -0,0 +1,7 @@
+package urushi
+
+type Tweener struct {
+
+}
+
+// func (t *Tweener) 
