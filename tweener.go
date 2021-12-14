36478738f27cbfd861c48c80c31ef834b6d34efe
Ichiban Kunio <kunio.ichiban@gmail.com>
2021-12-14 10:32:04 +0900

    fixed

--- tweener.go
+++ tweener.go
@@ -28,8 +28,8 @@ func NewTweener(start float64, dst float64, duration float64) *Tweener {
 }
 
 func (t *Tweener) Update(cc int) (float64, bool) {
-	// t.sigma ++
-	t.sigma += (t.dst-t.start) / t.duration
+	t.sigma ++
+	// t.sigma += (t.dst-t.start) / t.duration
 	value := easeIn(float64(cc), t.start, t.dst - t.start, t.sigma)
 	// t.delta = value - t.start
 
