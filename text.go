f1e1b6f4ef1787323041710d8ddbfe17edabc2ad
Ichiban Kunio <kunio.ichiban@gmail.com>
2021-12-17 16:36:12 +0900

    fixed

--- text.go
+++ text.go
@@ -81,7 +81,7 @@ func (t *TxtSpr) Draw(screen *ebiten.Image) {
 
 			op := &ebiten.DrawImageOptions{}
 			// op.GeoM.Translate(t.Spr.X + float64(t.PadLeft), t.Spr.Y+float64(t.PadUp))
-			op.GeoM.Translate(t.Spr.X + float64(t.PadLeft), t.Spr.Y+float64(t.Font.Metrics().Height.Ceil() - text.BoundString(t.Font, t.Txt).Bounds().Min.Y + t.PadUp))
+			op.GeoM.Translate(t.Spr.X + float64(t.PadLeft), t.Spr.Y+float64(- text.BoundString(t.Font, t.Txt).Bounds().Min.Y + t.PadUp))
 
 			// op.GeoM.Translate(t.Spr.X + float64(t.PadLeft), t.Spr.Y+float64(-t.Font.Metrics().Height.Ceil() + t.PadUp))
 			op.ColorM.Scale(colorToScale(t.Clr))
