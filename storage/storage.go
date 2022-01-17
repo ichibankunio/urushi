b37a2612d9b5a909f376e95900d5e3ca6e6f6232
Ichiban Kunio <kunio.ichiban@gmail.com>
2022-01-17 21:19:10 +0900

    fixed

---
+++ storage/storage.go
@@ -0,0 +1,100 @@
+package storage
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+	"syscall/js"
+)
+
+/*
+func Confirm(txt string) {
+	if js.Global().Get("window").Call("confirm", txt).Bool() {
+		js.Global().Get("location").Set("href", "index.html")
+	}
+}
+*/
+
+/*
+func SuggestApp() {
+	if strings.Contains(js.Global().Get("navigator").Get("userAgent").String(), "Android") {
+		// if r.MatchString(js.Global().Get("navigator").Get("userAgent").String()) {
+	// if js.Global().Get("navigator").Get("userAgent").match("/(iPhone|iPod|Android.*Mobile)/i)") {
+		if js.Global().Get("window").Call("confirm", "無料のアプリ版をダウンロードしませんか？\n全画面でサクサク動きます！\nPlayストアで開きますか？").Bool() {
+			js.Global().Get("location").Set("href", "https://play.google.com/store/apps/details?id=com.ku20298.hashidebashi&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1")
+			// if !js.Global().Get("window").Call("open", "https://play.google.com/store/apps/details?id=com.ku20298.hashidebashi").Bool() {
+			// }
+		}
+	}
+}
+*/
+
+func HideLoading() {
+	js.Global().Get("document").Call("getElementById", "loading").Get("style").Set("display", "none")
+}
+
+func IsMobile() bool {
+	r := regexp.MustCompile("iPhone|iPod|Android")
+	return r.MatchString(js.Global().Get("navigator").Get("userAgent").String()) 
+}
+
+func ShowAd() {
+	js.Global().Get("location").Set("href", "ad.html")
+}
+
+func Share() {
+	js.Global().Get("location").Set("href", "index.html")
+}
+
+// func ShowQR(this js.Value, args []js.Value) interface{} {
+//     js.Global().Call("alert", "hello")
+//     return nil
+// }
+
+
+func GetInt(tag string) int {
+	i, _ := strconv.Atoi(GetItem(tag).(js.Value).String())
+	return i
+}
+
+func GetItem(tag string) interface{} {
+	window := js.Global().Get("window")
+	localStorage := window.Get("localStorage")
+	
+	return localStorage.Get(tag)
+}
+
+func SetItem(tag string, a interface{}) {
+	window := js.Global().Get("window")
+	localStorage := window.Get("localStorage")
+	localStorage.Set(tag, a)
+	// js.Global().Get("localStorage").Call("setItem", a)
+}
+
+func SetArray(tag string, arr []int) {
+	arrStr := ""
+	for _, v := range arr {
+		arrStr += strconv.Itoa(v) + ","
+	}
+
+	SetItem(tag, arrStr)
+	// js.Global().Get("localStorage").Call("setItem", a)
+}
+
+func GetArray(tag string) []int {
+	arrStr := GetItem(tag).(js.Value).String()
+	
+	arr := []int{}
+	for _, v := range strings.Split(arrStr, ",") {
+		i, _ := strconv.Atoi(v)
+		arr = append(arr, i)
+	}
+	
+	return arr
+}
+
+func Clear() {
+	window := js.Global().Get("window")
+	localStorage := window.Get("localStorage")
+	localStorage.Call("clear")
+}
