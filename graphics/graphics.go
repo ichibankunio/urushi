409e332aebc3867476a30111b79be3c26d043dc0
Ichiban Kunio <kunio.ichiban@gmail.com>
2022-01-29 20:52:37 +0900

    fixed

--- graphics/graphics.go
+++ graphics/graphics.go
@@ -30,3 +30,29 @@ func DrawCircle(dst *ebiten.Image, cx, cy, r int, clr color.Color) {
 	}
 
 }
+func DrawCircleFilled(dst *ebiten.Image, x, y, r int, clr color.Color) {
+	cy := r
+	d := 3 - 2 * r
+	dst.Set(x, r + y, clr)
+	dst.Set(x, -r + y, clr)
+	dst.Set(r + x, y, clr)
+	dst.Set(-r + x, y, clr)
+
+	for cx := 0; cx <= cy; cx ++ {
+		if d < 0 {
+			d += 6 + 4 * cx
+		}else {
+			d += 10 + 4 * cx - 4 * (cy - 1)
+		}
+
+		dst.Set(cx + x, cy + y, clr)
+		dst.Set(cx - x, cy - y, clr)
+		dst.Set(cx - x, cy + y, clr)
+		dst.Set(cx + x, cy - y, clr)
+		dst.Set(cx + y, cy + x, clr)
+		dst.Set(cx - y, cy - x, clr)
+		dst.Set(cx - y, cy + x, clr)
+		dst.Set(cx + y, cy - x, clr)
+	}
+
+}
