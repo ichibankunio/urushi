27b94a85751551f0b400634cf32de9e06644bf89
Ichiban Kunio <kunio.ichiban@gmail.com>
2022-01-30 17:54:03 +0900

    fixed

--- ui/button.go
+++ ui/button.go
@@ -41,7 +41,7 @@ func (b *Button) Update() {
 	if b.spr.IsTouched() {
 		b.spr.Alpha = 0.5
 	}
-	if inpututil.IsTouchJustReleased(b.spr.TouchID) || (inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) && b.spr.IsTouched()) {
+	if inpututil.IsTouchJustReleased(b.spr.TouchID) || (inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft)) {
 		b.spr.Alpha = 1
 	}
 }
